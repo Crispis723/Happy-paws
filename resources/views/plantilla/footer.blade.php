4791005657ddfe459bf5ead42051102a49462816
Cristian <cristian@hotmail.com>
2025-12-13 16:03:44 -0500

    Subiendo proyecto Laravel inicial

---
+++ resources/views/plantilla/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="app-footer">
+    <!--begin::To the end-->
+    <div class="float-end d-none d-sm-inline">PROYECTOS-SENA</div>
+    <!--end::To the end-->
+    <!--begin::Copyright-->
+    <strong>
+        Copyright &copy; 2026&nbsp;
+        <a href="#" class="text-decoration-none">Sena - Adso</a>.
+    </strong>
+    All rights reserved.
+    <!--end::Copyright-->
+</footer>
